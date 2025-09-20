7f9e4992ab621638ae3903d04bdf511cefe2ea48
LuigiCanoro <phoenix_luigi@hotmail.com>
2025-09-20 14:21:12 -0300

    Adding enums to prevent errors when calling page object methods

---
+++ fixtures/enum.ts
@@ -0,0 +1,13 @@
+export enum ResultsLayoutOption {
+  GRID = 'grid',
+  LIST = 'list',
+  MAP = 'map'
+}
+
+export enum GuestScore {
+  EXCELLENT = 'Excellent (9+)',
+  VERY_GOOD = 'Very Good (7+)',
+  GOOD = 'Good (5+)',
+  AVERAGE = 'Average (5-)',
+  ALL = 'All',
+}
